5e61ac46baa88000a5e23b3b80d3954519b6fbd7
owen151128 <owen151128@gmail.com>
2018-03-23 14:53:59 +0900

    Service 로 floating widget 구현
    웹뷰 관련은 다음 PR에서 이루어질 예정

---
+++ app/src/main/java/kr/owens/drivetube/service/FloatingWidgetService.java
@@ -0,0 +1,118 @@
+package kr.owens.drivetube.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+
+import kr.owens.drivetube.R;
+import kr.owens.drivetube.databinding.WidgetFloatingBinding;
+import kr.owens.drivetube.ui.MainActivity;
+
+public class FloatingWidgetService extends Service {
+
+    private WidgetFloatingBinding binding;
+    private WindowManager mWindowManager;
+    private WindowManager.LayoutParams params;
+    private View mView;
+
+    private int initialX = 0;
+    private int initialY = 0;
+    private float initialTouchX = 0;
+    private float initialTouchY = 0;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        binding = DataBindingUtil.inflate(LayoutInflater.from(this), R.layout.widget_floating, null, false);
+        binding.setService(this);
+        mView = binding.getRoot();
+        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+        params.gravity = Gravity.TOP | Gravity.START;
+        params.x = 0;
+        params.y = 100;
+
+        mWindowManager.addView(mView, params);
+
+        binding.widgetIcon.setOnTouchListener((View v, MotionEvent e) -> {
+            switch (e.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    initialX = params.x;
+                    initialY = params.y;
+
+                    initialTouchX = e.getRawX();
+                    initialTouchY = e.getRawY();
+                    return true;
+
+                case MotionEvent.ACTION_UP:
+                    int xDiff = (int) (e.getRawX() - initialTouchX);
+                    int yDiff = (int) (e.getRawY() - initialTouchY);
+
+                    if (xDiff < 10 && yDiff < 10) {
+                        if (isViewWidget()) {
+                            binding.widgetLayout.setVisibility(View.GONE);
+                            binding.webViewLayout.setVisibility(View.VISIBLE);
+                        }
+                    }
+                    v.performClick();
+                    return true;
+
+                case MotionEvent.ACTION_MOVE:
+                    params.x = initialX + (int) (e.getRawX() - initialTouchX);
+                    params.y = initialY + (int) (e.getRawY() - initialTouchY);
+                    mWindowManager.updateViewLayout(mView, params);
+                    return true;
+            }
+            return false;
+        });
+
+        binding.widgetIcon.setOnLongClickListener(v -> {
+            Intent intent = new Intent(FloatingWidgetService.this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            stopSelf();
+
+            return false;
+        });
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mView != null)
+            mWindowManager.removeView(mView);
+    }
+
+    private boolean isViewWidget() {
+        return mView == null || binding.widgetLayout.getVisibility() == View.VISIBLE;
+    }
+
+    public void onStopServiceButtonClicked(View v) {
+        stopSelf();
+    }
+
+    public void onCloseWebViewButtonClicked(View v) {
+        binding.widgetLayout.setVisibility(View.VISIBLE);
+        binding.webViewLayout.setVisibility(View.GONE);
+    }
+}
